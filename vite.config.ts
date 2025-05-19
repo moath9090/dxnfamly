7c860b7e94614008cf5de90020c7547f6bb0a43f
Moaz90 <moath.qasim.alshmmari@gmail.com>
2025-05-19 20:28:00 +0300

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+ 
 export default defineConfig({
+  base: '/dxnfamly/', // 👈 هذا مهم جدًا عند النشر إلى GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
+
